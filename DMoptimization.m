ef694982ab642264ffca4684f6c5805c0cde6a0a
Hagai Har-Gil <hagaihargil@gmail.com>
2019-09-22 11:13:27 +0300

    Improved DM main script

--- DMoptimization.m
+++ DMoptimization.m
@@ -1,22 +1,21 @@
 function DMoptimization (src,evt,varargin)
 % User function for ScanImage, performs whenever a frame is acquired
-hSI=src.hSI;
-x=hSI.hDisplay.lastFrameNumber;
-if x==1
-    % Initialize data for pipeline
-    oldParams = zeros(1);
-    olderZernike=zeros(1,30);
-    oldZernike=GenerateRandVec(30,-10,0.1,10);
-else
-    % Import data for pipeline 
-    oldParams=importdata('oldParams.mat');
-    olderZernike=importdata('olderZernike.mat');
-    oldZernike=importdata('oldZernike.mat');
+hSI = src.hSI;
+persistent optimizationParams
+persistent oldZernikeVec
+persistent olderZernikeVec
+
+switch evt.EventName
+    case {'acqModeStart'}
+        % Initialize data for pipeline
+        optimizationParams = zeros(1);
+        olderZernikeVec = zeros(1,30);
+        oldZernikeVec = GenerateRandVec(30,-10,0.1,10);   
+        
+    case {'frameAcquired'}
+        % Call pipeline to recieve new image parameters
+        optimizationParams = pipeline(optimizationParams, oldZernikeVec, olderZernikeVec);
+        % Plot the new parameters for each frame taken
+        graphParams(hSI.hDisplay.lastFrameNumber, optimizationParams);
 end
-% Call pipeline to recieve new image parameters
-newParams = pipeline(oldParams,oldZernike,olderZernike);
-% Plot the new parameters for each frame taken
-graphParams(x,newParams);
-% Update image parameters
-save('oldParams.mat','newParams')
 end
