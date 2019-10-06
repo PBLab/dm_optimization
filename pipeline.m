b63c9ae9787582698781489222828b265c28c676
Nitzan <Nitzan@LAPTOP-35AFUEBB.mynet>
2019-10-06 21:59:56 +0300

    Updated pipeline to fit the genetic algorithm

--- pipeline.m
+++ pipeline.m
@@ -1,11 +1,24 @@
-function newParams = pipeline(img, oldParams,oldZernike,olderZernike)
-% Mock pipeline for operating the Alpao mirror
-    % Extract relevant parameters from the image
-    newParams = extractParams(img);
-    fprintf('The new params are: %s\n', newParams);
-    newZernike = testIfHigher(oldParams, newParams,oldZernike,olderZernike);
-    % Compares the old and new parameters of the images
-    % and sends an appropriate command to the mirror.
-    MirrorCommand(newZernike);
-    fprintf('Command sent\n');
+function [newPop, savedPop]=pipeline(frameNum,img,pop,genesNum,popSize)
+% Assigns fitness value to each Zernike vector and sends the next Zernike
+% vector to the mirror
+    newPop=pop;
+    if mod(frameNum,popSize)~=0
+        newPop(mod(frameNum,popSize),genesNum+1)=fitnessFun(img);
+        savedPop=newPop;
+        if mod(frameNum+1,popSize)~=0
+            MirrorCommand(newPop(mod(frameNum+1,popSize),1:genesNum));
+            fprintf('Command sent\n');
+        else
+            MirrorCommand(newPop(popSize,1:genesNum));
+            fprintf('Command sent\n');
+        end
+    else
+         newPop(popSize,genesNum+1)=fitnessFun(img);
+         savedPop=newPop;
+         disp(savedPop)
+         % Call genetic algorithm to recieve new population
+         newPop = geneticAlgorithm(newPop,genesNum,popSize);
+         MirrorCommand(newPop(1,1:genesNum));
+         fprintf('Command sent\n');            
+    end
 end
