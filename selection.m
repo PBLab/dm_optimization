f65cf270da0b9c1e93f89a6b6f18c8088e2f887b
Nitzan <Nitzan@LAPTOP-35AFUEBB.mynet>
2019-10-06 21:38:19 +0300

    initial commit

---
+++ selection.m
@@ -0,0 +1,16 @@
+function [parent1, parent2]=selection(oldPop,popSize,genesNum)
+% Performs tournament parent selection
+k=2;    % Number of parents competing
+for i=1:2
+    bestInd=[];
+    for j=1:k
+        ind=oldPop(randi([1 popSize],1,1),:);
+        if isempty(bestInd)==1 || bestInd(1,genesNum+1)<ind(1,genesNum+1)
+            bestInd=ind;
+        end
+    end
+    parents(i,:)=bestInd;
+end
+parent1 = parents(1,:);
+parent2 = parents(2,:);
+end
