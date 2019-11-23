a6de73e5803ab161f57bac853f9ce7766f1c005d
Nitzan <nitzanh1995@gmail.com>
2019-11-23 17:56:21 +0200

    Instead of calculating the mean intensity, it calculates the mean FWHM of the image PSF spots

--- fitnessFun.m
+++ fitnessFun.m
@@ -1,6 +1,16 @@
-function params = fitnessFun(img)
-% For a given image, calculates the mean value (fitness) of all
-% pixels in the image
+function params = fitnessFun(image)
+% For a given image, calculates the mean FWHM (fitness) of all PSF spots in 
+% the image
 
-params = mean(img(:));
+% Extract only the spots from the image
+spots = pickSpot(image);
+fields = fieldnames(spots);
+% For each spot, calculate FWHM of its horizontal signal (identical to the
+% vertical)
+FWHMs = zeros(1,length(fields));
+for i = 1:length(fields)
+[horizontalSig,verticalSig] = extractSignal(image,spots.(fields{i}));
+FWHMs(i) = extractFWHM(horizontalSig);
+end
+params = mean(FWHMs);
 end
