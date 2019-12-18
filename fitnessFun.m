966b2d708e233d831272d820b2a53526ccc80e98
Nitzan <nitzanh1995@gmail.com>
2019-12-18 20:09:55 +0200

    Small updates

--- fitnessFun.m
+++ fitnessFun.m
@@ -3,14 +3,18 @@ function params = fitnessFun(image)
 % the image
 
 % Extract only the spots from the image
-spots = pickSpot(image);
+threshold = 70; % The percentile below which there's a backround 
+width = 40; % Radius used to create a disk-shaped structuring element
+spots = findPsfSpots(image, threshold, width);
+
+% For each spot, calculate mean FWHM of its horizontal and vertical signals
 fields = fieldnames(spots);
-% For each spot, calculate FWHM of its horizontal signal (identical to the
-% vertical)
 FWHMs = zeros(1,length(fields));
 for i = 1:length(fields)
-[horizontalSig,verticalSig] = extractSignal(image,spots.(fields{i}));
-FWHMs(i) = extractFWHM(horizontalSig);
+    [horizontalSig,verticalSig] = extractSignal(image,spots.(fields{i}));
+    FWHMs(i) = mean([extractFWHM(horizontalSig,'transpose',1)...
+        extractFWHM(verticalSig)]);
 end
+% Claculate mean FWHM of all spots 
 params = mean(FWHMs);
 end
