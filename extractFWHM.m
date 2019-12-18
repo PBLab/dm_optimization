f86b835b5078c47732eb68182b31a4946ee7efd1
Nitzan <nitzanh1995@gmail.com>
2019-12-18 20:06:49 +0200

    New method to extract FWHM

--- extractFWHM.m
+++ extractFWHM.m
@@ -1,25 +1,22 @@
-function  FWHM = extractFWHM(signal)
-% Extracts the FWHM of a given signal
+function  FWHM = extractFWHM(signal,varargin)
+% Extracts FWHM of a given signal by fitting a gaussian shape to the signal 
 
-% Normalize the signal from 0 to 1
-normSig = signal/max(signal);
-% Find the X index of the gaussian maximum
-x_max = find(normSig == 1);
-firstHalf = normSig(1:x_max);
-secondHalf = normSig(x_max+1:length(normSig));
-% Find the Y value closest to 0.5 in every half of the gaussian
-higher = [min(firstHalf(firstHalf >= 0.5)),min(secondHalf(secondHalf >= 0.5))];
-lower = [max(firstHalf(firstHalf <= 0.5)),max(secondHalf(secondHalf <= 0.5))];
-y = zeros(1,2);
-for i = 1:2
-    if abs(higher(i)-0.5) <= abs(lower(i)-0.5)
-        y(i) = higher(i);
-    else
-        y(i) = lower(i);
-    end
+% Optional- transpose the signal
+f1=find(strcmp('transpose',varargin));
+if ~isempty(f1)
+    transFlag = varargin{f1+1};
+else
+    transFlag = 0;
 end
-% Find the X index of Y~0.5
-x1 = find(firstHalf == y(1));
-x2 = find(secondHalf == y(2))+x_max;
-FWHM=x2-x1;
+
+% Apply a gaussian fitting
+x=1:length(signal);
+if transFlag == 1
+    f = fit(x.',signal.','gauss1');
+else
+    f = fit(x.',signal,'gauss1');
+end
+std = (f.c1)/sqrt(2);
+% Relation between standard deviation and FWHM
+FWHM= std * 2 * sqrt(2*log(2));                                 
 end
