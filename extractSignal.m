a6db6c45577025282d84d96fc77f08e9363f591c
Nitzan <nitzanh1995@gmail.com>
2019-12-18 20:07:43 +0200

    Small corrections

--- extractSignal.m
+++ extractSignal.m
@@ -1,9 +1,10 @@
 function [horizontalSig,verticalSig] = extractSignal(image,spot,varargin)
 % Extracts the horizontal and vertical gaussian signals from a PSF spot
+
 rows = spot(:,1);
 cols = spot(:,2);
-horizontalSig = image(floor(median(rows)),min(cols):max(cols));
-verticalSig = image(min(rows):max(rows),floor(median(cols)));
+horizontalSig = image(floor(mean(rows)),min(cols):max(cols));
+verticalSig = image(min(rows):max(rows),floor(mean(cols)));
 % Optional- apply gaussian filter
 f1=find(strcmp('filter',varargin));
 if ~isempty(f1)
@@ -13,6 +14,6 @@ else
 end
 if filterFlag == 1
     horizontalSig = smoothdata(horizontalSig,'gaussian',20);
-    verticalSig = smoothdata(horizontalSig,'gaussian',20);
+    verticalSig = smoothdata(verticalSig,'gaussian',20);
 end
 end
